7d3051946e18edece9c81f01a4b043a479081d6a
arbaz <arbaz7668@gmail.com>
2025-01-27 11:08:20 +0530

    added

--- s3-bucket.tf
+++ s3-bucket.tf
@@ -4,7 +4,7 @@ provider "aws" {
 }
 
 resource "aws_s3_bucket" "example_bucket" {
-  bucket = "my-example-bucket" # Replace with your unique bucket name
+  bucket = "loud-specialone" # Replace with your unique bucket name
 
   tags = {
     Environment = "Dev"
